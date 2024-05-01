159e86565dd2ed7a26c3d39b3ba7663521263318
Miao Li <miaoli.cs@gmail.com>
2024-05-01 16:40:29 +0100

    update

--- analysis/evaluating_generations.py
+++ analysis/evaluating_generations.py
@@ -17,27 +17,28 @@ if __name__ == "__main__":
     scores_zs, scores_conv = summac_scores([document], [summary])
     print("scores zs", np.mean(scores_zs), "scores conv", np.mean(scores_conv))
 
-    # evaluating human-written meta-reviews
-    output_file = "/data/gpfs/projects/punim0521/MistralX/results/mistral_7b_instruct_v02_peersum/predictions_zeroshot.jsonl"
-    documents = []
-    summaries = []
-    samples = []
-    with jsonlines.open(output_file) as reader:
-        for line in reader:
-            samples.append(line)
-            documents.append("\n".join(line["source"]))
-            summaries.append(line["summary"])
-
-    scores_zs, scores_conv = summac_scores(documents, summaries)
-    print(np.mean(scores_zs), np.mean(scores_conv))
-
-    results = []
-    for sample, zs, conv in zip(samples, scores_zs, scores_conv):
-        sample["summac_zs"] = zs
-        sample["summac_conv"] = conv
-        results.append(sample)
-    with jsonlines.open("scores_peersum_human_written.jsonl", "w") as writer:
-        writer.write_all(samples)
+    # # evaluating human-written meta-reviews
+    # output_file = "/data/gpfs/projects/punim0521/MistralX/results/mistral_7b_instruct_v02_peersum/predictions_zeroshot.jsonl"
+    # documents = []
+    # summaries = []
+    # samples = []
+    # with jsonlines.open(output_file) as reader:
+    #     for line in reader:
+    #         samples.append(line)
+    #         documents.append("\n".join(line["source"]))
+    #         summaries.append(line["summary"])
+    #
+    # scores_zs, scores_conv = summac_scores(documents, summaries)
+    # print(np.mean(scores_zs), np.mean(scores_conv))
+    #
+    # results = []
+    # for sample, zs, conv in zip(samples, scores_zs, scores_conv):
+    #     sample["summac_zs"] = zs
+    #     sample["summac_conv"] = conv
+    #     results.append(sample)
+    # with jsonlines.open("scores_peersum_human_written.jsonl", "w") as writer:
+    #     writer.write_all(samples)
+    # # -0.36093002860045564 0.49920685880405957
 
     # # evaluating human-written meta-reviews
     # output_file = "/data/gpfs/projects/punim0521/MistralX/results/mistral_7b_instruct_v02_peersum/predictions_zeroshot.jsonl"
@@ -62,26 +63,26 @@ if __name__ == "__main__":
     #     writer.write_all(samples)
     # # -0.2107997606339326 0.48214757963272425
 
-    # # evaluating human-written meta-reviews
-    # output_file = "/data/gpfs/projects/punim0521/MistralX/results/mistral_7b_instruct_v02_peersum/predictions_finetuned.jsonl"
-    # documents = []
-    # summaries = []
-    # samples = []
-    # with jsonlines.open(output_file) as reader:
-    #     for line in reader:
-    #         samples.append(line)
-    #         documents.append("\n".join(line["source"]))
-    #         summaries.append(line["generation"])
-    #
-    # scores_zs, scores_conv = summac_scores(documents, summaries)
-    # print(np.mean(scores_zs), np.mean(scores_conv))
-    #
-    # results = []
-    # for sample, zs, conv in zip(samples, scores_zs, scores_conv):
-    #     sample["summac_zs"] = zs
-    #     sample["summac_conv"] = conv
-    #     results.append(sample)
-    # with jsonlines.open("scores_peersum_mistral_7b_instruct_v02_finetuned.jsonl", "w") as writer:
-    #     writer.write_all(samples)
+    # evaluating human-written meta-reviews
+    output_file = "/data/gpfs/projects/punim0521/MistralX/results/mistral_7b_instruct_v02_peersum/predictions_finetuned.jsonl"
+    documents = []
+    summaries = []
+    samples = []
+    with jsonlines.open(output_file) as reader:
+        for line in reader:
+            samples.append(line)
+            documents.append("\n".join(line["source"]))
+            summaries.append(line["generation"])
+
+    scores_zs, scores_conv = summac_scores(documents, summaries)
+    print(np.mean(scores_zs), np.mean(scores_conv))
+
+    results = []
+    for sample, zs, conv in zip(samples, scores_zs, scores_conv):
+        sample["summac_zs"] = zs
+        sample["summac_conv"] = conv
+        results.append(sample)
+    with jsonlines.open("scores_peersum_mistral_7b_instruct_v02_finetuned.jsonl", "w") as writer:
+        writer.write_all(samples)
 
 
