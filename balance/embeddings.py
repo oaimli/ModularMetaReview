fa0025cddba28f3f356b3bba9295edfc55e003e1
Miao Li <miaoli.cs@gmail.com>
2024-05-01 17:48:22 +0100

    update

--- balance/embeddings.py
+++ balance/embeddings.py
@@ -39,7 +39,7 @@ for human_written_sample, zeroshot_sample, finetuned_sample in zip(human_written
         human_written_sample["mistral_7b_instruct_v02_finetuned"] = finetuned_sample["generation"]
         samples.append(human_written_sample)
 
-samples = random.sample(samples, 512)
+samples = random.sample(samples, 96)
 
 results = []
 for i, sample in tqdm(enumerate(samples), total=len(samples)):
