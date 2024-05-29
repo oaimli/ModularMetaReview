640c621c65fa0e749350117db8a2da00cc71a0f0
Miao Li <miaoli.cs@gmail.com>
2024-05-29 15:59:51 +0100

    update

--- peermeta/parsing/download_pdf.py
+++ peermeta/parsing/download_pdf.py
@@ -1,6 +1,7 @@
 import os
 import wget
 import jsonlines
+from tqdm import tqdm
 
 
 conference = "iclr_2018"
@@ -9,11 +10,15 @@ pdf_folder = f"../data/pdfs_{conference}"
 existing_ids = []
 for file in os.listdir(pdf_folder):
     existing_ids.append(file[:-4])
+print(existing_ids)
 
+pdfs = {}
 with jsonlines.open(f"../data/{conference}.jsonl") as reader:
     for line in reader:
-        id = line["id"]
-        if id not in existing_ids:
-            url = line["pdf"]
-            wget.download(url, out=f"{pdf_folder}/{id}.pdf")
+        pdfs[line["id"]] = line["pdf"]
+
+for id in tqdm(pdfs.keys()):
+    if id not in existing_ids:
+        url = pdfs[id]
+        wget.download(url, out=f"{pdf_folder}/{id}.pdf")
 
