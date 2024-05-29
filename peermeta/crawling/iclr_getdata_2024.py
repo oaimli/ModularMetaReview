1b08d5d1c1a40c579d48227ce3a3718d07b2eb34
Miao Li <miaoli.cs@gmail.com>
2024-05-29 11:32:29 +0100

    update

--- peermeta/crawling/iclr_getdata_2024.py
+++ peermeta/crawling/iclr_getdata_2024.py
@@ -32,7 +32,7 @@ for note in tqdm(notes):
     paper["id"] = note.forum
     paper["venue"] = content['venueid']["value"]
     if "ICLR.cc/2024/Conference/Desk_Rejected_Submission" != paper["venue"]:
-        paper["pdf"] = "https://openreview.net" + content["pdf"]["value"]
+        paper["pdf"] = "https://openreview.net" + content.get("pdf", {"value": ""})["value"]
         rcs = client.get_notes(forum=paper["id"])
         reviews_commments = []
         for rc in rcs:
