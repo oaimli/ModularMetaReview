db714dcb6b8153380515d1f2dbbf5405eb4659e6
Miao Li <miaoli.cs@gmail.com>
2024-05-29 09:33:19 +0100

    update

--- peermeta/crawling/iclr_getdata_before.py
+++ peermeta/crawling/iclr_getdata_before.py
@@ -3,7 +3,7 @@ import random
 import jsonlines
 
 if __name__ == "__main__":
-    data_folder_peersum = "/home/miao4/punim0521/NeuralAbstractiveSummarization/ideas/peersum/crawling_data/data"
+    data_folder_peersum = "/home/miao4/punim0521/NeuralAbstractiveSummarization/ideas/peersum/crawling_data/data/"
     conference = "iclr_2018"
     with open(data_folder_peersum + f"{conference}.json") as f:
         papers = json.load(f)
