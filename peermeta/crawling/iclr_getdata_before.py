3ff9a042fb83b8185c1be36d07ace079b6cc4b8a
Miao Li <miaoli.cs@gmail.com>
2024-05-29 09:33:58 +0100

    update

--- peermeta/crawling/iclr_getdata_before.py
+++ peermeta/crawling/iclr_getdata_before.py
@@ -9,20 +9,20 @@ if __name__ == "__main__":
         papers = json.load(f)
     print(len(papers))
 
-    papers_new = []
-    for paper in papers:
-        if paper.get("paper_id", "") == "":
-            print("####")
-        if paper.get("paper_title", "") == "":
-            print("####")
-        if paper.get("paper_abstract", "") == "":
-            print("####")
-        if paper.get("paper_acceptance", "") == "":
-            print("####")
-        if paper.get("meta_review", "") == "":
-            print("####")
-        if len(paper.get("reviews", [])) == 0:
-            print("####")
-
-    with jsonlines.open(f"../data/{conference}.jsonl", "w") as writer:
-        writer.write_all(papers_new)
+    # papers_new = []
+    # for paper in papers:
+    #     if paper.get("paper_id", "") == "":
+    #         print("####")
+    #     if paper.get("paper_title", "") == "":
+    #         print("####")
+    #     if paper.get("paper_abstract", "") == "":
+    #         print("####")
+    #     if paper.get("paper_acceptance", "") == "":
+    #         print("####")
+    #     if paper.get("meta_review", "") == "":
+    #         print("####")
+    #     if len(paper.get("reviews", [])) == 0:
+    #         print("####")
+    #
+    # with jsonlines.open(f"../data/{conference}.jsonl", "w") as writer:
+    #     writer.write_all(papers_new)
