fc556020d642c73230ad430e5d9f29a8c6c1ddc3
Miao Li <miaoli.cs@gmail.com>
2024-06-12 16:47:52 +0100

    update

--- peermeta/parsing/download_pdfs_cr.py
+++ peermeta/parsing/download_pdfs_cr.py
@@ -13,8 +13,11 @@ pdf_folder = f"../data/pdfs_cr_{conference}"
 
 existing_ids = []
 for file in os.listdir(pdf_folder):
+    # If the file name is not normal, remove the file
     if len(file) > 15:
         print(file)
+        os.remove(os.path.join(pdf_folder, file))
+
     existing_ids.append(file[:-4])
 print(len(existing_ids))
 
