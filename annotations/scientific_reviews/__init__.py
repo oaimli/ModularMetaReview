337fd6101538173fcd6682f4e06c79da591176c1
Miao Li <miaoli.cs@gmail.com>
2024-07-08 08:33:58 +0100

    update

--- annotations/scientific_reviews/__init__.py
+++ annotations/scientific_reviews/__init__.py
@@ -1,2 +1,2 @@
-# The annotations are from MetaReviewLogic/annotation_analysis
-# The annotation samples are from MetaReviewLogic/annotation_data
+# The annotated data from two annotators are from MetaReviewLogic/annotation_analysis
+# The annotation samples are from MetaReviewLogic/annotation_data and there are 30 samples
---
+++ annotations/scientific_reviews/transforming_data.py
@@ -0,0 +1,4 @@
+# To make it easy to evaluate the categorization results
+# We have to transform the annotated data into the same format as our automatic extracted fragments
+
+import jsonlines
