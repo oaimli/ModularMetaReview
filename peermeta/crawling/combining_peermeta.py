1f9191b649ca5225834c3ea131d700d581a267df
Miao Li <miaoli.cs@gmail.com>
2024-07-21 16:55:55 +0100

    update

--- peermeta/crawling/combining_peermeta.py
+++ peermeta/crawling/combining_peermeta.py
@@ -333,7 +333,7 @@ def prepare_iclr(year):
                     paper_new["paper_acceptance"] = paper["venue"]
                     final_decision_content = paper["final_decision"]["content"]
                     final_decision_time = paper["final_decision"]["tmdate"]
-                    final_decision = final_decision_content["comment"]
+                    final_decision = final_decision_content["metareview:_summary,_strengths_and_weaknesses"]
                     if final_decision != "":
                         paper_new["meta_review"] = final_decision
 
@@ -470,10 +470,10 @@ def prepare_iclr(year):
                     paper_new["paper_id"] = "iclr_" + year + "_" + paper["id"]
                     paper_new["paper_title"] = paper["title"]
                     paper_new["paper_abstract"] = paper["abstract"]
-                    paper_new["paper_acceptance"] = paper["comment"]
+                    paper_new["paper_acceptance"] = paper["venue"]
                     final_decision_content = paper["final_decision"]["content"]
                     final_decision_time = paper["final_decision"]["tmdate"]
-                    final_decision = final_decision_content["comment"]
+                    final_decision = final_decision_content["metareview:_summary,_strengths_and_weaknesses"]
                     if final_decision != "":
                         paper_new["meta_review"] = final_decision
 
