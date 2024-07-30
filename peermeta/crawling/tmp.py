04cb2988e07fcf87f9330212cef29e2e79b9c13f
Miao Li <miaoli.cs@gmail.com>
2024-07-30 16:33:59 +0100

    update

--- peermeta/crawling/tmp.py
+++ peermeta/crawling/tmp.py
@@ -1,6 +1,11 @@
+import json
 import jsonlines
 
 
+samples = []
 with jsonlines.open("../data/iclr_2023.jsonl") as reader:
     for line in reader:
         print(line)
+
+with open("tmp.json", "w") as f:
+    json.dump(samples, f)
