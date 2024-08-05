985f5cd284aae599285ba3546e530f639ff41f48
Miao Li <miaoli.cs@gmail.com>
2024-08-05 12:10:07 +0100

    update

--- led_ft/papers/tmp.py
+++ led_ft/papers/tmp.py
@@ -1,4 +1,4 @@
 from datasets import load_dataset
 
-dataset_all = load_dataset('json', data_files='../../peermeta/peermeta_train.jsonl', split='all')
+dataset_all = load_dataset('json', data_files='../../datasets/peermeta_train.jsonl', split='all')
 print("dataset all", len(dataset_all))
