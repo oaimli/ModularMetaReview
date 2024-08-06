2115c38cfa04a892e3239c3418072b9649a9c863
Miao Li <miaoli.cs@gmail.com>
2024-08-06 10:20:57 +0100

    update

--- llama3_ft/papers/bash/finetune_debug.sh
+++ llama3_ft/papers/bash/finetune_debug.sh
@@ -30,7 +30,6 @@ torchrun --nnodes 1 --nproc_per_node=4 --master_port=9822 llama_finetune.py  \
                 --save_steps 2 \
                 --save_total_limit 1 \
                 --load_best_model_at_end True \
-                --save_on_each_node False \
                 --metric_for_best_model eval_loss \
                 --greater_is_better False \
                 --optim adafactor \
