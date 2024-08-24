64dd33eb5ffe4ed00ac7a33145b02092fdbeb96a
Miao Li <miaoli.cs@gmail.com>
2024-08-24 23:02:20 +0100

    update

--- eval_auto/adjusted_summac/scoring.py
+++ eval_auto/adjusted_summac/scoring.py
@@ -14,62 +14,6 @@ def summac_scores(sources, targets):
     return score_zs["scores"], score_conv["scores"]
 
 
-def llama3_prompting(input_text: str, facet: str, mode: str = "meta"):
-    print("#######")
-    prompt_format = open(f"{prompt_folder}/prompt_{mode.lower()}_{facet.lower()}.txt").read()
-    prompt_content = prompt_format.replace("{{input_document}}", input_text)
-    # print(prompt_format)
-    while True:
-        try:
-            output_dict = client.chat.completions.create(
-                model="meta-llama/Meta-Llama-3.1-70B-Instruct",
-                messages=[
-                    {"role": "system", "content": "You are requested to do some extraction work. You must output the answer following the format of the example output, without any other useless content."},
-                    {"role": "user",
-                     "content": prompt_content}
-                    ],
-                n=10
-                )
-
-            all_candidates = []
-            all_candidates_len = []
-            tmp = []
-            for choice in output_dict.choices:
-                output_content = choice.message.content
-                if "no related fragments" not in output_content.lower():
-                    content_len = len(output_content.split())
-                    all_candidates_len.append(content_len)
-                    tmp.append(content_len)
-                    all_candidates.append(output_content.split("\n"))
-            if len(all_candidates) < 5:
-                outputs = []
-            else:
-                tmp.sort()
-                outputs = all_candidates[all_candidates_len.index(tmp[int(len(tmp) / 2)])]
-            break
-        except Exception as e:
-            print(e)
-            if ("limit" in str(e)):
-                time.sleep(2)
-
-    return outputs
-
-
-def categorizing_meta_review(meta_review: str) -> Dict:
-    """
-    Args:
-        meta_review: the meta-review of a sample
-        model_name: the name of a model
-    Returns:
-        result: a dictionary of extracted fragments for different review facets
-    """
-    result = {}
-    for facet in facets:
-        result[facet] = llama3_prompting(meta_review, facet, "meta")
-
-    return result
-
-
 if __name__ == "__main__":
     model_name = "llama31_70b"
     openai_api_key = "EMPTY"
@@ -86,17 +30,6 @@ if __name__ == "__main__":
     for dataset_name in dataset_names:
         print(dataset_name)
 
-        facets = []
-        if dataset_name == "peermeta":
-            facets = ["Novelty", "Soundness", "Clarity", "Advancement", "Compliance", "Overall"]
-            prompt_folder = "../optimization/organization/prompts_scientific"
-        if dataset_name == "space":
-            facets = ["Building", "Cleanliness", "Food", "Location", "Rooms", "Service"]
-            prompt_folder = "../modular_llama3/hotels/prompts_organization"
-        if dataset_name == "amasum_shoes":
-            facets = ["Breathability", "Durability", "Weight", "Cushioning", "Stability", "Flexibility", "Traction", "Sizefit", "Comfort", "Misc"]
-            prompt_folder = "../modular_llama3/shoes/prompts_organization"
-
         generations_info = info[dataset_name]
         for generation_info in generations_info:
             generation_file = generation_info["generation_file"]
@@ -104,12 +37,17 @@ if __name__ == "__main__":
             candidate_key = generation_info["candidate_key"]
             reference_key = generation_info["reference_key"]
             source_key = "source_documents"
-            with open(generation_file) as f:
+
+            # use the processed result with shared content from categorization
+            shared_file = "_".join(generation_file.split("/")[1:]).split(".")[0] + "_shared.json"
+            with open(shared_file) as f:
                 samples = json.load(f)
 
             candidates = []
             references = []
             source_texts = []
+            references_shared = []
+            candidates_shared = []
             for sample in samples:
                 candidates.append(sample[candidate_key])
                 if isinstance(sample[reference_key], str):
@@ -118,25 +56,13 @@ if __name__ == "__main__":
                     references.append(sample[reference_key][0])  # SPACE has multiple references
                 source_texts.append("\n".join(sample[source_key]))
 
+                references_shared.append(sample[reference_key + "_shared"])
+                candidates_shared.append(sample[candidate_key + "_shared"])
+
             # compared with source texts
             scores_zs_source, scores_conv_source = summac_scores(source_texts, candidates)
             score_zs_source_avg = np.mean(scores_zs_source)
             score_conv_source_avg = np.mean(scores_conv_source)
-
-            # compared with references on only shared aspects
-            references_shared = []
-            candidates_shared = []
-            for reference, candidate in zip(references, candidates):
-                categorization_reference = categorizing_meta_review(reference)
-                categorization_candidate = categorizing_meta_review(candidate)
-                reference_shared = []
-                candidate_shared = []
-                for facet in facets:
-                    if len(categorization_reference[facet]) > 0 and len(categorization_candidate[facet]) > 0:
-                        reference_shared.extend(categorization_reference[facet])
-                        candidate_shared.extend(categorization_candidate[facet])
-                reference_shared.append(" ".join(reference_shared))
-                candidates_shared.append(" ".join(candidate_shared))
 
             scores_zs_reference, scores_conv_reference = summac_scores(references_shared, candidates_shared)
             score_zs_reference_avg = np.mean(scores_zs_reference)
@@ -145,4 +71,4 @@ if __name__ == "__main__":
             print("scores zs:", "source", score_zs_source_avg, "reference", score_zs_reference_avg, "summation",
                   score_zs_source_avg + score_zs_reference_avg)
             print("scores conv:", "source", score_conv_source_avg, "reference", score_conv_reference_avg, "summation",
-          score_conv_source_avg + score_conv_reference_avg)
+                  score_conv_source_avg + score_conv_reference_avg)
