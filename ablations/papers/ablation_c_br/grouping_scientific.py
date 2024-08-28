9c6028ed9d57ff3479e7e151a08dd65d6fdaffc8
Miao Li <miaoli.cs@gmail.com>
2024-08-28 11:02:49 +0100

    update

---
+++ ablations/papers/ablation_c_br/grouping_scientific.py
@@ -0,0 +1,38 @@
+import json
+
+
+with open("../../../optimization/organization/scientific_categorization_result_llama31_70b_processed.json") as f:
+    processed_samples = json.load(f)
+
+with open("../../../annotations/scientific_reviews/br_annotation_result_fragments.json") as f:
+    bryan_results = json.load(f)
+
+shared_ids = list(
+    set(bryan_results.keys()).intersection(set(processed_samples.keys())))
+print(len(shared_ids))
+
+for key in shared_ids:
+    sample = processed_samples[key]
+    sample_human = bryan_results[key]
+
+    meta_review_categorization = sample["meta_review_categorization"]
+    review_categorizations = sample_human["review_categorization"]
+
+    categorization_pairs = []
+    for facet in meta_review_categorization.keys():
+        tmp = {}
+        tmp["facet"] = facet
+
+        review_fragments = []
+        for review_categorization in review_categorizations:
+            review_fragments.extend(review_categorization[facet])
+        tmp["review_fragments"] = review_fragments
+        tmp["meta_review_fragments"] = meta_review_categorization[facet]
+
+
+        categorization_pairs.append(tmp)
+
+    processed_samples[key]["categorization_pairs"] = categorization_pairs
+
+with open("scientific_selection_result_llama31_70b.json", "w") as f:
+    json.dump(processed_samples, f, indent=4)
---
+++ ablations/papers/ablation_c_br/prompting_scientific_llama3_reasoning.py
@@ -0,0 +1,73 @@
+from openai import OpenAI
+import time
+import json
+from tqdm import tqdm
+from typing import List
+
+
+def llama3_prompting(review_fragments: List):
+    prompt_format = open("prompt_reasoning_scientific.txt").read()
+    review_text = "\n".join(review_fragments)
+    prompt_content = prompt_format.replace("{{review_fragments}}", review_text)
+    # print(prompt_format)
+    while True:
+        try:
+            output_dict = client.chat.completions.create(
+                model="meta-llama/Meta-Llama-3.1-70B-Instruct",
+                messages=[
+                    {"role": "system", "content": "Always answer with only the summary, without other content."},
+                    {"role": "user",
+                     "content": prompt_content}
+                    ],
+                n=8
+                )
+            all_candidates = []
+            all_candidates_len = []
+            tmp = []
+            for choice in output_dict.choices:
+                output_content = choice.message.content
+                content_len = len(output_content.split())
+                all_candidates_len.append(content_len)
+                tmp.append(content_len)
+                all_candidates.append(output_content)
+            tmp.sort()
+            meta_generated = all_candidates[all_candidates_len.index(tmp[int(len(tmp) / 2)])]
+            break
+        except Exception as e:
+            print(e)
+            if ("limit" in str(e)):
+                time.sleep(2)
+    print(meta_generated)
+    return meta_generated
+
+
+def facet_reasoning(categorization_pairs: List) -> List:
+    result = []
+    for pair in categorization_pairs:
+        review_fragments = pair["review_fragments"]
+        pair["meta_generated"] = llama3_prompting(review_fragments)
+        result.append(pair)
+
+    return result
+
+
+if __name__ == "__main__":
+    model_name = "llama31_70b"
+    openai_api_key = "EMPTY"
+    openai_api_base = "http://localhost:8000/v1"
+    client = OpenAI(api_key=openai_api_key, base_url=openai_api_base)
+
+    with open("scientific_selection_result_llama31_70b.json") as f:
+        test_samples = json.load(f)
+
+    results = {}
+    for key in tqdm(test_samples):
+        sample = test_samples[key]
+        categorization_pairs = sample["categorization_pairs"]
+        sample["categorization_pairs"] = facet_reasoning(categorization_pairs)
+        results[key] = sample
+        # print(sample)
+
+    print(len(results))
+    with open(f"scientific_reasoning_result_{model_name}.json", "w") as f:
+        json.dump(results, f, indent=4)
