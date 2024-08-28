b1f6deb89771324ee02af28b5f2676a92790f9f1
Miao Li <miaoli.cs@gmail.com>
2024-08-28 23:57:48 +0100

    length difference

--- analysis/length_difference.py
+++ analysis/length_difference.py
@@ -1,12 +1,50 @@
 import json
 import numpy as np
+from rouge_score import rouge_scorer
+from nltk.tokenize import sent_tokenize
+
+
+def rouge(reference, candidate, types=None, use_stemmer=True, split_summaries=True):
+    """
+    This is based on rouge-score 0.0.4
+    If using rougeLsum, it is necessary to split sentences with '\n' in summaries in advance
+    """
+    if types == None:
+        types = ['rouge1', 'rouge2', 'rougeL', 'rougeLsum']
+
+    if 'rougeLsum' in types and split_summaries:
+        reference = '\n'.join(sent_tokenize(reference))
+        candidate = '\n'.join(sent_tokenize(candidate))
+
+    results = {}
+    for t in types:
+        if t not in ['rouge1', 'rouge2', 'rougeL', 'rougeLsum']:
+            print("The type must be selected in rouge1, rouge2, rougeL, and rougeLsum.")
+            return results
+    scorer = rouge_scorer.RougeScorer(types, use_stemmer=use_stemmer)
+    scores = scorer.score(reference, candidate)
+    for t in types:
+        r = {}
+        r["precision"] = scores[t].precision
+        r["recall"] = scores[t].recall
+        r["fmeasure"] = scores[t].fmeasure
+        results[t] = r
+    return results
 
 
 def analyzing(targets, sources):
-    lengths = []
-    for target in targets:
-        lengths.append(len(target.split()))
-    print("Mean", np.mean(lengths), "Max", np.max(lengths), "Min", np.min(lengths), "Std", np.std(lengths))
+    # lengths = []
+    # for target in targets:
+    #     lengths.append(len(target.split()))
+    # print("Mean", np.mean(lengths), "Max", np.max(lengths), "Min", np.min(lengths), "Std", np.std(lengths))
+
+    rouges = []
+    for target, source in zip(targets, sources):
+        scores = rouge(target, source)
+        rouges.append((scores["rouge1"]["fmeasure"] + scores["rouge2"]["fmeasure"] + scores["rougeLsum"]["fmeasure"]) / 3)
+    print("Mean", np.mean(rouges), "Max", np.max(rouges), "Min", np.min(rouges), "Std", np.std(rouges))
+
+
 
 
 
