90ac001be73b1bc5dd692bd23dc23b92ac000fad
Miao Li <miaoli.cs@gmail.com>
2024-10-22 10:13:37 +0100

    updated

---
+++ eval_auto/aspect_coverage_truncated.py
@@ -0,0 +1,138 @@
+import numpy as np
+import json
+
+
+if __name__ == "__main__":
+    with open("info.json") as f:
+        info = json.load(f)
+
+    dataset_names = ["peermeta", "space", "amasum_shoes"]
+    for dataset_name in dataset_names:
+        print(dataset_name)
+
+        facets = []
+        if dataset_name == "peermeta":
+            facets = ["Novelty", "Soundness", "Clarity", "Advancement", "Compliance", "Overall"]
+        if dataset_name == "space":
+            facets = ["Building", "Cleanliness", "Food", "Location", "Rooms", "Service"]
+        if dataset_name == "amasum_shoes":
+            facets = ["Breathability", "Durability", "Weight", "Cushioning", "Stability", "Flexibility", "Traction",
+                      "Sizefit", "Comfort", "Misc"]
+
+        generations_info = info[dataset_name]
+
+        # all generation results share the same review-categorization and there must be the modular row in the batch
+        generation_file_target = ""
+        for generation_info in generations_info:
+            if generation_info["model_name"] == "modular_llama3":
+                generation_file_target = generation_info["generation_file"]
+                generation_file_target = generation_file_target[:-5] + "_truncated.json"
+        categorization_file = "_".join(generation_file_target.split("/")[1:]).split(".")[0] + ".json"
+        with open("categorization/" + categorization_file) as f:
+            samples = json.load(f)
+        review_categorizations = []
+        for sample in samples:
+            review_categorizations.append(sample["review_categorization"])
+
+
+        # human reference
+        generation_file = generations_info[0]["generation_file"]
+        generation_file = generation_file[:-5] + "_truncated.json"
+        print("human reference")
+        candidate_key = generations_info[0]["candidate_key"]
+        reference_key = generations_info[0]["reference_key"]
+
+        categorization_file = "_".join(generation_file.split("/")[1:]).split(".")[0] + ".json"
+        with open("categorization/" + categorization_file) as f:
+            samples = json.load(f)
+
+        candidates = []
+        references = []
+        reference_categorizations = []
+        for sample in samples:
+            candidates.append(sample[candidate_key])
+            if isinstance(sample[reference_key], str):
+                references.append(sample[reference_key])
+            else:
+                references.append(sample[reference_key][0])  # SPACE has multiple references
+            reference_categorizations.append(sample["categorization_reference"])
+
+        recalls = []
+        precisions = []
+        f_measures = []
+        for review_categorization, reference_categorization in zip(review_categorizations, reference_categorizations):
+            review_count = 0
+            shared_count = 0
+            candidate_count = 0
+            for facet in facets:
+                flag = 0
+                for categorization in review_categorization:
+                    if len(categorization[facet]) > 0:
+                        flag = 1
+                        break
+                if flag == 1:
+                    review_count += 1
+
+                if len(reference_categorization[facet]) > 0:
+                    candidate_count += 1
+
+                if flag == 1 and len(reference_categorization[facet]) > 0:
+                    shared_count += 1
+            r = (shared_count + 1) / (review_count + 1)
+            p = (shared_count + 1) / (candidate_count + 1)
+            recalls.append(r)
+            precisions.append(p)
+            f_measures.append(2 * (r * p) / (r + p))
+        print("recall", np.mean(recalls), "precision", np.mean(precisions), "f-measure", np.mean(f_measures))
+
+        # model generations
+        for generation_info in generations_info:
+            generation_file = generation_info["generation_file"]
+            generation_file = generation_file[:-5] + "_truncated.json"
+            print(generation_file)
+            candidate_key = generation_info["candidate_key"]
+            reference_key = generation_info["reference_key"]
+
+            # use the processed result with shared content from categorization
+            categorization_file = "_".join(generation_file.split("/")[1:]).split(".")[0] + ".json"
+            with open("categorization/" + categorization_file) as f:
+                samples = json.load(f)
+
+            candidates = []
+            references = []
+            candidate_categorizations = []
+            for sample in samples:
+                candidates.append(sample[candidate_key])
+                if isinstance(sample[reference_key], str):
+                    references.append(sample[reference_key])
+                else:
+                    references.append(sample[reference_key][0])  # SPACE has multiple references
+                candidate_categorizations.append(sample["categorization_candidate"])
+
+            recalls = []
+            precisions = []
+            f_measures = []
+            for review_categorization, candidate_categorization in zip(review_categorizations, candidate_categorizations):
+                review_count = 0
+                shared_count = 0
+                candidate_count = 0
+                for facet in facets:
+                    flag = 0
+                    for categorization in review_categorization:
+                        if len(categorization[facet]) > 0:
+                            flag = 1
+                            break
+                    if flag == 1:
+                        review_count += 1
+
+                    if len(candidate_categorization[facet]) > 0:
+                        candidate_count += 1
+
+                    if flag == 1 and len(candidate_categorization[facet]) > 0:
+                        shared_count += 1
+                r = (shared_count + 1) / (review_count + 1)
+                p = (shared_count + 1) / (candidate_count + 1)
+                recalls.append(r)
+                precisions.append(p)
+                f_measures.append(2 * (r * p) / (r + p))
+            print("recall", np.mean(recalls), "precision", np.mean(precisions), "f-measure", np.mean(f_measures))
---
+++ eval_auto/rouge_fmeasure_truncated.py
@@ -0,0 +1,95 @@
+from rouge_score import rouge_scorer
+from nltk.tokenize import sent_tokenize
+import numpy as np
+import json
+
+
+def rouge(reference, candidate, types=None, use_stemmer=True, split_summaries=True):
+    """
+    This is based on rouge-score 0.0.4
+    If using rougeLsum, it is necessary to split sentences with '\n' in summaries in advance
+    """
+    if types == None:
+        types = ['rouge1', 'rouge2', 'rougeL', 'rougeLsum']
+
+    if 'rougeLsum' in types and split_summaries:
+        reference = '\n'.join(sent_tokenize(reference))
+        candidate = '\n'.join(sent_tokenize(candidate))
+
+    results = {}
+    for t in types:
+        if t not in ['rouge1', 'rouge2', 'rougeL', 'rougeLsum']:
+            print("The type must be selected in rouge1, rouge2, rougeL, and rougeLsum.")
+            return results
+    scorer = rouge_scorer.RougeScorer(types, use_stemmer=use_stemmer)
+    scores = scorer.score(reference, candidate)
+    for t in types:
+        r = {}
+        r["precision"] = scores[t].precision
+        r["recall"] = scores[t].recall
+        r["fmeasure"] = scores[t].fmeasure
+        results[t] = r
+    return results
+
+
+def rouge_corpus(references, candidates, types=None, use_stemmer=True, split_summaries=True):
+    assert len(references) == len(candidates)
+
+    if types == None:
+        types = ['rouge1', 'rouge2', 'rougeL', 'rougeLsum']
+
+    results = {}
+    for t in types:
+        s = {}
+        s['recall'] = []
+        s['precision'] = []
+        s['fmeasure'] = []
+        results[t] = s
+    for ref, can in zip(references, candidates):
+        s = rouge(ref, can, types=types, use_stemmer=use_stemmer, split_summaries=split_summaries)
+        for t in types:
+            results[t]['recall'].append(s[t]['recall'])
+            results[t]['precision'].append(s[t]['precision'])
+            results[t]['fmeasure'].append(s[t]['fmeasure'])
+
+    final_results = {}
+    for t in types:
+        s = results[t]
+        tmp = {}
+        tmp['precision'] = np.mean(s['precision'])
+        tmp['recall'] = np.mean(s['recall'])
+        tmp['fmeasure'] = np.mean(s['fmeasure'])
+        final_results[t] = tmp
+    return final_results
+
+
+if __name__ == "__main__":
+    with open("info.json") as f:
+        info = json.load(f)
+
+    dataset_names = ["peermeta", "space", "amasum_shoes"]
+    for dataset_name in dataset_names:
+        print(dataset_name)
+        generations_info = info[dataset_name]
+        for generation_info in generations_info:
+            generation_file = generation_info["generation_file"]
+            generation_file = generation_file[:-5] + "_truncated.json"
+            print(generation_file)
+            candidate_key = generation_info["candidate_key"]
+            reference_key = generation_info["reference_key"]
+            with open(generation_file) as f:
+                samples = json.load(f)
+
+            candidates = []
+            references = []
+            for sample in samples:
+                candidates.append(sample[candidate_key])
+                if isinstance(sample[reference_key], str):
+                    references.append(sample[reference_key])
+                else:
+                    references.append(sample[reference_key][0]) # SPACE has multiple references
+
+            scores = rouge_corpus(references, candidates, types=['rouge1', 'rouge2', 'rougeLsum'])
+
+            print("Average F1",
+                  (scores["rouge1"]["fmeasure"] + scores["rouge2"]["fmeasure"] + scores["rougeLsum"]["fmeasure"]) / 3)
