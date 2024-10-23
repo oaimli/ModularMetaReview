ddf9ee7fd232d481b31fa0f2f3e8309ced9027d7
Miao Li <miaoli.cs@gmail.com>
2024-10-23 22:44:37 +0100

    updated

--- eval_auto/adjusted_summac_full.py
+++ eval_auto/adjusted_summac_full.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     with open("info.json") as f:
         info = json.load(f)
 
-    dataset_names = ["peermeta", "space", "amasum_shoes"]
+    dataset_names = ["space", "peermeta", "amasum_shoes"]
     for dataset_name in dataset_names:
         scores_source = {}
         print(dataset_name)
