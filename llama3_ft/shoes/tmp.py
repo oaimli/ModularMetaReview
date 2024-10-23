0905502390cc96c3ae2bba2d10aefaf35e4e9af6
Miao Li <miaoli.cs@gmail.com>
2024-10-23 17:57:00 +0100

    updated

--- llama3_ft/shoes/tmp.py
+++ llama3_ft/shoes/tmp.py
@@ -1,10 +1,18 @@
-import jsonlines
-import json
+# import jsonlines
+# import json
+#
+# results = []
+# with jsonlines.open("/home/miao4/punim0521/ModularMetaReview/results/llama31_8b_amasum_shoes/generated_summaries.jsonl") as reader:
+#     for line in reader:
+#         results.append(line)
+#
+# with open("/home/miao4/punim0521/ModularMetaReview/results/llama31_8b_amasum_shoes/generated_summaries.json", "w") as f:
+#     json.dump(results, f)
 
-results = []
-with jsonlines.open("/home/miao4/punim0521/ModularMetaReview/results/llama31_8b_amasum_shoes/generated_summaries.jsonl") as reader:
-    for line in reader:
-        results.append(line)
 
-with open("/home/miao4/punim0521/ModularMetaReview/results/llama31_8b_amasum_shoes/generated_summaries.json", "w") as f:
-    json.dump(results, f)
+import torch
+from transformers import AutoModelForCausalLM
+
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3.1-8B", trust_remote_code=True, torch_dtype=torch.bfloat16,
+                                                 device_map="auto", attn_implementation="flash_attention_2")
+print(model.config)
