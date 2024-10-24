cceb1854f7515711986deaa73d40b916531b65f5
Miao Li <miaoli.cs@gmail.com>
2024-10-24 15:01:31 +0100

    updated

--- eval_auto/alignscore_full.py
+++ eval_auto/alignscore_full.py
@@ -4,7 +4,7 @@ from alignscore import AlignScore
 
 
 if __name__ == "__main__":
-    scorer = AlignScore(model='roberta-base', batch_size=32, device='cuda:0', ckpt_path='/path/to/checkpoint',
+    scorer = AlignScore(model='roberta-large', batch_size=32, device='cuda:0', ckpt_path='../tmp/AlignScore/AlignScore-large.ckpt',
                         evaluation_mode='nli_sp')
     model_name = "llama31_70b"
 
