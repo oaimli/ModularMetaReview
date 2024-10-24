03bc4da207d8a7ab0b07057b2e4d657822bd417a
Miao Li <miaoli.cs@gmail.com>
2024-10-24 16:21:20 +0100

    updated

--- eval_auto/alignscore_full.py
+++ eval_auto/alignscore_full.py
@@ -17,31 +17,33 @@ if __name__ == "__main__":
 
         generations_info = info[dataset_name]
 
-        # # human reference
-        # generation_file = generations_info[0]["generation_file"]
-        # print("human reference")
-        # reference_key = generations_info[0]["reference_key"]
-        # source_key = "source_documents"
-        # with open(generation_file) as f:
-        #     samples = json.load(f)
-        # references = []
-        # source_texts = []
-        # for sample in samples:
-        #     if isinstance(sample[reference_key], str):
-        #         references.append(sample[reference_key])
-        #     else:
-        #         references.append(sample[reference_key][0])  # SPACE has multiple references
-        #     source_texts.append("\n".join(sample[source_key]))
-        # # compared with source texts
-        # scores_align = scorer.score(contexts=source_texts, claims=references)
-        # print(scores_align)
-        # scores_source["human_reference"] = scores_align
-        # print("scores alignscore:", np.mean(scores_align))
+        # human reference
+        generation_file = generations_info[0]["generation_file"]
+        print("human reference")
+        reference_key = generations_info[0]["reference_key"]
+        source_key = "source_documents"
+        with open(generation_file) as f:
+            samples = json.load(f)
+        references = []
+        source_texts = []
+        for sample in samples:
+            if isinstance(sample[reference_key], str):
+                references.append(sample[reference_key])
+            else:
+                references.append(sample[reference_key][0])  # SPACE has multiple references
+            source_texts.append("\n".join(sample[source_key]))
+        # compared with source texts
+        scores_align = scorer.score(contexts=source_texts, claims=references)
+        print(scores_align)
+        scores_source["human_reference"] = scores_align
+        print("scores alignscore:", np.mean(scores_align))
 
         # model generations
         for generation_info in generations_info:
             generation_file = generation_info["generation_file"]
             print(generation_file)
+            with open(generation_file) as f:
+                samples = json.load(f)
             candidate_key = generation_info["candidate_key"]
             source_key = "source_documents"
 
