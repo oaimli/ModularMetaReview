3609d46a761ef056b26edaaa5f3a02427ea94676
Miao Li <miaoli.cs@gmail.com>
2024-10-28 23:49:16 +0000

    updated

--- eval_auto/eyeballing.py
+++ eval_auto/eyeballing.py
@@ -1,7 +1,6 @@
 import numpy as np
 import json
 
-
 if __name__ == "__main__":
     with open("info.json") as f:
         info = json.load(f)
@@ -12,31 +11,41 @@ if __name__ == "__main__":
         print(dataset_name)
         with open(f"{dataset_name}_summac_source_full.json") as f:
             all_scores_summac_source = json.load(f)
-
+        with open(f"{dataset_name}_alignscore_full.json") as f:
+            all_scores_alignscore_source = json.load(f)
+        with open(f"llm_score/{dataset_name}_llm_scored_full.json") as f:
+            all_scores_llm_score = json.load(f)
+        with open(f"mini_score/{dataset_name}_llm_scored_full.json") as f:
+            all_scores_mini_score = json.load(f)
 
         generations_info = info[dataset_name]
         outputs_combined = {}
-        scores_summac_source_reference = all_scores_summac_source["human_reference"]
         for generation_info in generations_info:
             generation_file = generation_info["generation_file"]
+            with open(generation_file) as f:
+                samples = json.load(f)
+            assert len(samples) == len(all_scores_summac_source) == len(all_scores_alignscore_source) == len(
+                all_scores_llm_score) == len(all_scores_mini_score)
             print(generation_file)
+
+            scores_summac_source_reference = all_scores_summac_source["human_reference"]
+            scores_summac_source_generation = all_scores_summac_source[generation_file]
+            scores_alignscore_reference = all_scores_alignscore_source["human_reference"]
+
 
             candidate_key = generation_info["candidate_key"]
             reference_key = generation_info["reference_key"]
-            scores_summac_source_generation = all_scores_summac_source[generation_file]
-
-            with open(generation_file) as f:
-                samples = json.load(f)
             i = 0
-            for sample, score_generation, score_reference in zip(samples, scores_summac_source_generation, scores_summac_source_reference):
+            for sample, score_generation_summac_source, score_reference_summac_source in zip(samples, scores_summac_source_generation,
+                                                                                             scores_summac_source_reference):
                 tmp = outputs_combined.get(str(i), {})
                 if isinstance(sample[reference_key], str):
-                    tmp["reference"] = [sample[reference_key], score_reference]
+                    tmp["reference"] = [sample[reference_key], score_reference_summac_source]
                 else:
-                    tmp["reference"] = [sample[reference_key][0], score_reference]
+                    tmp["reference"] = [sample[reference_key][0], score_reference_summac_source]
 
                 existing_candidates = tmp.get("generations", [])
-                existing_candidates.append((generation_info["model_name"], sample[candidate_key], score_generation))
+                existing_candidates.append({"model": generation_info["model_name"], "generation": sample[candidate_key], "summac-source": score_generation_summac_source})
                 tmp["generations"] = existing_candidates
                 tmp["source_documents"] = sample["source_documents"]
                 outputs_combined[str(i)] = tmp
