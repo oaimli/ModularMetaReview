590842b44ba8d8710216f79cbc8281d00f12833b
Miao Li <miaoli.cs@gmail.com>
2024-10-28 11:14:53 +0000

    updated

--- eval_auto/mini_score/scoring.py
+++ eval_auto/mini_score/scoring.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
         info = json.load(f)
 
     # dataset_names = ["space", "peermeta", "amasum_shoes"]
-    dataset_names = ["space", "peermeta"]
+    dataset_names = ["amasum_shoes"]
     for dataset_name in dataset_names:
         print(dataset_name)
         generation_infos = info[dataset_name]
