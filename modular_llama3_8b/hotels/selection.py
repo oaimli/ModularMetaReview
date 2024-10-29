9fc56ed9874ecb518753be632e3eb7b0d5f63ce7
Miao Li <miaoli.cs@gmail.com>
2024-10-29 23:12:20 +0000

    updated

---
+++ modular_llama3_8b/hotels/selection.py
@@ -0,0 +1,26 @@
+# This is just to group extracted fragments into different review facets
+import json
+
+
+model_name = "llama31_8b"
+with open(f"space_categorization_result_{model_name}.json") as f:
+    processed_samples = json.load(f)
+
+for sample_index, sample in enumerate(processed_samples):
+    review_categorizations = sample["review_categorization"]
+
+    categorization_pairs = []
+    for facet in review_categorizations[0].keys():
+        tmp = {}
+        tmp["facet"] = facet
+
+        review_fragments = []
+        for review_categorization in review_categorizations:
+            review_fragments.extend(review_categorization[facet])
+        tmp["review_fragments"] = review_fragments
+        categorization_pairs.append(tmp)
+
+    processed_samples[sample_index]["categorization_pairs"] = categorization_pairs
+
+with open(f"space_selection_result_{model_name}.json", "w") as f:
+    json.dump(processed_samples, f, indent=4)
---
+++ modular_llama3_8b/shoes/reasoning.py
@@ -0,0 +1,75 @@
+from openai import OpenAI
+import time
+import json
+from tqdm import tqdm
+from typing import List
+
+
+def gpt4_prompting(review_fragments: List):
+    prompt_format = open("prompts_reasoning/prompt_reasoning.txt").read()
+    review_text = "\n".join(review_fragments)
+    prompt_content = prompt_format.replace("{{review_fragments}}", review_text)
+    # print(prompt_format)
+    while True:
+        try:
+            output_dict = client.chat.completions.create(
+                model="meta-llama/Llama-3.1-8B-Instruct",
+                messages=[
+                    {"role": "system", "content": "Always answer with only the summary, without other content."},
+                    {"role": "user",
+                     "content": prompt_content}
+                    ],
+                n=8
+                )
+            all_candidates = []
+            all_candidates_len = []
+            tmp = []
+            for choice in output_dict.choices:
+                output_content = choice.message.content
+                content_len = len(output_content.split())
+                all_candidates_len.append(content_len)
+                tmp.append(content_len)
+                all_candidates.append(output_content)
+            tmp.sort()
+            meta_generated = all_candidates[all_candidates_len.index(tmp[int(len(tmp) / 2)])]
+            break
+        except Exception as e:
+            print(e)
+            if ("limit" in str(e)):
+                time.sleep(2)
+    print(meta_generated )
+    return meta_generated
+
+
+def facet_reasoning(categorization_pairs: List) -> List:
+    result = []
+    for pair in categorization_pairs:
+        review_fragments = pair["review_fragments"]
+        pair["meta_generated"] = gpt4_prompting(review_fragments)
+        result.append(pair)
+
+    return result
+
+
+if __name__ == "__main__":
+    model_name = "llama31_8b"
+    openai_api_key = "EMPTY"
+    openai_api_base = "http://localhost:8000/v1"
+    client = OpenAI(
+        api_key=openai_api_key,
+        base_url=openai_api_base,
+        )
+
+    with open(f"amasum_shoes_selection_result_{model_name}.json") as f:
+        test_samples = json.load(f)
+
+    results = []
+    for sample in tqdm(test_samples):
+        categorization_pairs = sample["categorization_pairs"]
+        sample["categorization_pairs"] = facet_reasoning(categorization_pairs)
+        results.append(sample)
+        # print(sample)
+
+    print(len(results))
+    with open(f"amasum_shoes_reasoning_result_{model_name}.json", "w") as f:
+        json.dump(results, f, indent=4)
