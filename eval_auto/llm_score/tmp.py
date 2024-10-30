2a31fa40137368adffb51f2afc72db2a69c9f932
Miao Li <miaoli.cs@gmail.com>
2024-10-30 21:53:59 +0000

    updated

--- eval_auto/llm_score/tmp.py
+++ eval_auto/llm_score/tmp.py
@@ -1,7 +1,7 @@
 import json
 import numpy as np
 
-with open("space_llm_scored_full.json") as f:
+with open("space_llm_scored_full_tmp.json") as f:
     results = json.load(f)
 
 for model, result in results.items():
