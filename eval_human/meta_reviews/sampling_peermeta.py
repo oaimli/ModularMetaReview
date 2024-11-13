34777508bad5620446b04433e179ef37f7908bd3
Miao Li <miaoli.cs@gmail.com>
2024-11-13 10:31:14 +0000

    updated

--- eval_human/meta_reviews/sampling_peermeta.py
+++ eval_human/meta_reviews/sampling_peermeta.py
@@ -78,7 +78,7 @@ for generation_info in generation_infos:
             tmp = []
             for source in sample_test["source_documents"]:
                 tmp.append(source["content"])
-            if sample_test["meta_review"] == generation[reference_key] and tmp == source_documents:
+            if sample_test["meta_review"] == generation[reference_key]:
                 paper_id = sample_test["paper_id"]
                 sample_new["paper_id"] = paper_id
                 # source documents with conversational structures
