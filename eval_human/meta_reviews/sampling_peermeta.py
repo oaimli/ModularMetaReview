563e53ce9069468150df2437a10e79e5d04c7382
Miao Li <miaoli.cs@gmail.com>
2024-11-13 09:42:06 +0000

    updated

--- eval_human/meta_reviews/sampling_peermeta.py
+++ eval_human/meta_reviews/sampling_peermeta.py
@@ -38,7 +38,7 @@ indexes = []
 for sample_key, sample_value in samples.items():
     source_documents = sample_value["source_documents"]
     source_text_length = len("\n".join(source_documents).split())
-    if len(source_documents) <= 8 and source_text_length < 3000:
+    if len(source_documents) <= 8 and source_text_length < 4000:
         indexes.append(sample_key)
 print(len(indexes))
 indexes_sampled = random.sample(indexes, 20)
